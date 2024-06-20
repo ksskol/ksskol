a305857900a1c78a71cf36fb793b639ba95c85cc
ksskol <skolotakristina2@gmail.com>
2024-06-20 15:50:14 +0100

    nav bar for a phone

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,52 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { IoReorderThree } from "react-icons/io5";
+import { VscChromeClose } from "react-icons/vsc";
 
 export default function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function handleClick() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  function handleLinkClick() {
+    setIsMenuOpen(false);
+  }
+
   return (
-    <header>
-      <Link to={`/`}>ksskol</Link>
-      <div>
-        <Link to={`/about`}>
-          about
+    <div className="block">
+      <div className="flex items-center justify-between p-2">
+        <Link className="font-medium" to={`/`}>
+          ksskol
         </Link>
-        <Link to={`/projects`}> 
-          projects
-        </Link>
-        <Link to={`/resume`}>
-          resume
-        </Link>
+
+        <button onClick={handleClick} className="flex items-center text-2xl">
+          {isMenuOpen ? <VscChromeClose /> : <IoReorderThree />}
+        </button>
+
+        {isMenuOpen && (
+          <div className="absolute right-10 top-16 z-10 w-8/12 p-4 border border-gray-950 bg-white shadow-md rounded-md">
+            <div className="grid grid-cols-1 space-y-5">
+              <button className="p-2 flex">
+                <Link to={`/about`} onClick={handleLinkClick}>
+                  about
+                </Link>
+              </button>
+              <button className="p-2 flex">
+                <Link to={`/projects`} onClick={handleLinkClick}>
+                  projects
+                </Link>
+              </button>
+              <button className="p-2 flex">
+                <Link to={`/resume`} onClick={handleLinkClick}>
+                  resume
+                </Link>
+              </button>
+            </div>
+          </div>
+        )}
       </div>
-    </header>
+    </div>
   );
 }
